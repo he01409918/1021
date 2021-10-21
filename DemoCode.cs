fbc65ea91e2b37db08a5b01b664fcb92f28e3564
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-10-21 08:57:35 +0800

    Add files via upload

---
+++ DemoCode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoCode : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.LogError("OnTriggerEnter");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}

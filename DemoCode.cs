33b4ad09d8eb174332bc445030553710be1e3674
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-10-21 09:25:01 +0800

    Update DemoCode.cs

--- DemoCode.cs
+++ DemoCode.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DemoCode : MonoBehaviour
 {
+
+    public int curretScore;
+    public Text ScoreText;
+
     public void OnTriggerEnter(Collider other)
     {
         Debug.LogError("OnTriggerEnter");
@@ -13,10 +18,14 @@ public class DemoCode : MonoBehaviour
     {
         if (collision.collider.tag == "02")
         {
+            curretScore += 5;
+            ScoreText.text = curretScore.ToString();
             Debug.LogError("我碰到吐司了");
         }
         if (collision.collider.name == "BaseFloor")
         {
+            curretScore -= 10;
+            ScoreText.text = curretScore.ToString();
             Debug.LogError("我碰到地板了");
         }
     }

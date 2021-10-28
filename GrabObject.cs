0018947d7e36deb860422e7d73442c66a29a29e8
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-10-28 09:25:12 +0800

    Add files via upload

---
+++ GrabObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrabObject : MonoBehaviour
+{
+    public Text ScoreText;
+
+    public float currentScore;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Rock")
+        {
+            TargetScore otherScore = collision.gameObject.GetComponent<TargetScore>();
+            currentScore += otherScore.myScore;
+            ScoreText.text = currentScore.ToString();
+        }
+    }
+}

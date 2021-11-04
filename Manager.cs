cc4cf7cc59acfdf4d07a31485d31a20ee2e45a10
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-04 09:42:16 +0800

    Add files via upload

---
+++ Manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    public GameObject Obj;
+
+    public Transform Point;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewObj();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewObj()///生成
+    {
+        GameObject obj = Instantiate(Obj , Point.position, Point.rotation);
+        Destroy(obj, 5);
+        Invoke("NewObj", 3);
+    }
+}

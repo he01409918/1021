837147ceb6b792d59c5c6873fc0336f6d07ae46b
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-04 09:50:49 +0800

    Update GrabObject.cs

--- GrabObject.cs
+++ GrabObject.cs
@@ -1,21 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GrabObject : MonoBehaviour
 {
-    public Text ScoreText;
-
-    public float currentScore;
-
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag == "Rock")
         {
             TargetScore otherScore = collision.gameObject.GetComponent<TargetScore>();
-            currentScore += otherScore.myScore;
-            ScoreText.text = currentScore.ToString();
+            Manager.Instance.UpdateScore(otherScore.myScore);
         }
     }
 }

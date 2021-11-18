5bfe8f463e67587e65226ed839434f2e2ebb2106
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-18 09:41:23 +0800

    Update Gun.cs

--- Gun.cs
+++ Gun.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
 public class Gun : VRTK_InteractableObject
 {
+
+    public GameObject Bullet;
+    public Transform FirePoint;
+    public float Power;
     public override void StartUsing(VRTK_InteractUse currentUsingObject = null)
     {
         base.StartUsing(currentUsingObject);
+
+        GameObject bullet = Instantiate(Bullet, FirePoint.position, FirePoint.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(FirePoint.forward * Power);
         Debug.Log("按下板機");
     }
 }

a7cdd46ba8b4b7d4eb6abbe86f3730f49a3ef5a2
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-18 09:24:08 +0800

    Add files via upload

---
+++ Gun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Gun : VRTK_InteractableObject
+{
+    public override void StartUsing(VRTK_InteractUse currentUsingObject = null)
+    {
+        base.StartUsing(currentUsingObject);
+        Debug.Log("按下板機");
+    }
+}

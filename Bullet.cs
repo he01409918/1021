db75bae9db8f60026cfe32be53a756ea1a359c2c
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-18 10:16:24 +0800

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject Fx;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(Fx, transform.position, transform.rotation);
+    }
+}

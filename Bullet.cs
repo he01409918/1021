5e9a781bec74e0419d9841e6ad9b624af4113d57
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-25 09:51:39 +0800

    Update Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -6,9 +6,15 @@ public class Bullet : MonoBehaviour
 {
     public GameObject Fx;
 
+    public float damage;
+
     private void OnCollisionEnter(Collision collision)
     {
         Instantiate(Fx, transform.position, transform.rotation);
+        if (collision.collider.CompareTag("Ai"))
+        {
+            collision.collider.GetComponent<Ai>().OnGetHit(damage);
+        }
         Destroy(gameObject);
     }
 }

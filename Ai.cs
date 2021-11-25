e7fc0145e68fab12519483c1d5bd5eff91e8bb92
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-25 09:04:42 +0800

    Add files via upload

---
+++ Ai.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Ai : MonoBehaviour
+{
+    private NavMeshAgent nav;
+    public Transform point;
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        nav.SetDestination(point.position);
+    }
+}

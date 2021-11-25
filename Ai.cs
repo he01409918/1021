7467903677dcd9ef35038cbec50aa347ca57967f
he01409918 <92556503+he01409918@users.noreply.github.com>
2021-11-25 09:43:23 +0800

    Update Ai.cs

--- Ai.cs
+++ Ai.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 using UnityEngine.AI;
 public class Ai : MonoBehaviour
 {
+    private Animator anim;
     private NavMeshAgent nav;
     public Transform point;
+
+    public float Hp;
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         nav.SetDestination(point.position);
+        anim.SetBool("movement", nav.remainingDistance > nav.stoppingDistance ? true : false);
     }
+
 }
